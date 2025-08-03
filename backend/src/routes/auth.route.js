4524578213466680c4850aea7763819836a54798
Monametsi <elboncaptain@gmail.com>
2025-08-03 20:48:39 +0200

    fix: correct import statements and add checkAuth route to auth router

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,6 +1,6 @@
 import express from "express";
-import {  signup, login, logout } from "../controllers/auth.controller.js";
-import { updateProfile } from "../../../../../Users/seelem/OneDrive - Botswana Telecommunications Company/Documents/SoftwareEngineering/fullstack-chat-app/backend/src/controllers/auth.controller.js";
+import {  signup, login, logout, updateProfile, checkAuth } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 
 const router = express.Router();
@@ -10,5 +10,6 @@ router.post("/login", login);
 router.post("/logout", logout);
 router.put("/update-profile", protectRoute, updateProfile);
 
+router.get("/check", protectRoute, checkAuth);
 
 export default router;
