9b8feb76194848dfa244d24ebee16564d6ab28e9
Monametsi <elboncaptain@gmail.com>
2025-08-03 20:48:05 +0200

    fix: correct import and usage of messageRoutes in index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,8 @@ import authRoute from "./routes/auth.route.js";
 import dotenv from "dotenv";
 import { connectDB} from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+
 import cookieParser from "cookie-parser";
 dotenv.config();
 
@@ -10,7 +12,9 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 const PORT = process.env.PORT
-app.use("/api/auth", authRoute);
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
+
 app.listen(PORT, () => {
     console.log("Server running on port " + PORT);
     connectDB();
