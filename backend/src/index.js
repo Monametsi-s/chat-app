0aba403db90f326e262a5ef76e48ff46e1faa28d
Monametsi <elboncaptain@gmail.com>
2025-08-05 12:16:04 +0200

    Enable CORS on backend to allow requests from the frontend.

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import { connectDB} from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import cors from "cors"
 
 import cookieParser from "cookie-parser";
 dotenv.config();
@@ -11,6 +12,11 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+})
+);
 const PORT = process.env.PORT
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
