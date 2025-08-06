7d41e7581c1c132fc80a7a8decd83ad5fb81a519
Monametsi <elboncaptain@gmail.com>
2025-08-06 16:14:39 +0200

    feat(useAuthStore): add signup, logout, and checkAuth methods with loading states

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -32,5 +32,15 @@ export const useAuthStore = create((set) => ({
         } finally {
             set({ isSigningUp: false})
         }
+    },
+    logout: async () => {
+        try {
+            await axiosInstance.post("/auth/logout");
+            set({ authUser: null });
+            toast.success("logged out successfully")
+
+        } catch (error) {
+            toast.error(error.response.data.message)
+        }
     }
 }))
