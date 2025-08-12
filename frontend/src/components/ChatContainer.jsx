2a73ed7a68640dd9568688ff87bceffd76b1acb1
Monametsi <elboncaptain@gmail.com>
2025-08-12 11:29:57 +0200

    Implement ChatContainer with message loading and skeleton UI

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import { useChatStore } from "../store/useChatStore";
+import { useEffect } from "react";
+import ChatHeader from "./ChatHeader";
+import MessageInput from "./MessageInput"
+import MessageSkeleton from "./skeletons/MessageSkeleton";
 
-function ChatContainer() {
-  return (
-    <div>ChatContainer</div>
+const ChatContainer = () => {
+  const { messages, getMessages, isMessagesLoading, selectedUser } = useChatStore();
+
+  useEffect(() => {
+    getMessages(selectedUser._id)},[selectedUser._id, getMessages]
   )
-}
+  if (isMessagesLoading) return (
+  <div className="flex-1 flex flex-color overflow-auto">
+    <ChatHeader />
+    <MessageSkeleton />
+    {/* <MessageInput /> */}
+  </div>
+)
 
-export default ChatContainer
+  return (
+     <div className="flex-1 flex flex-col overflow-auto">
+        <ChatHeader />
+        {/* <MessageSkeleton /> */}
+        <MessageInput />
+        <p> messages... </p>
+      </div>
+  ) 
+}
+export default ChatContainer;
