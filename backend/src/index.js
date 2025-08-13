9aff6509ece0fcda38dd77351f63fd77e79d26d1
Monametsi <elboncaptain@gmail.com>
2025-08-13 20:12:32 +0200

    Fix: change app to server for socket handling in index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,11 +5,11 @@ import { connectDB} from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import cors from "cors"
+import { app, server } from  "./lib/socket.js";
 
 import cookieParser from "cookie-parser";
 dotenv.config();
 
-const app = express();
 // app.use(express.json());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
@@ -23,7 +23,7 @@ const PORT = process.env.PORT
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log("Server running on port " + PORT);
     connectDB();
 })
