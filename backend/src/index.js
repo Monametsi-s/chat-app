862b30d0b3da2345053833e6b90433a52ea5249e
Monametsi <elboncaptain@gmail.com>
2025-08-17 01:17:30 +0200

    refactor: improve code formatting and structure in index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,29 +1,39 @@
 import express from "express";
 import authRoute from "./routes/auth.route.js";
 import dotenv from "dotenv";
-import { connectDB} from "./lib/db.js";
+import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
-import cors from "cors"
-import { app, server } from  "./lib/socket.js";
-
+import cors from "cors";
+import { app, server } from "./lib/socket.js";
 import cookieParser from "cookie-parser";
+
+import path from "path";
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
+
 dotenv.config();
 
 // app.use(express.json());
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cookieParser());
-app.use(cors({
+app.use(
+  cors({
     origin: "http://localhost:5173",
     credentials: true,
-})
+  })
 );
-const PORT = process.env.PORT
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 server.listen(PORT, () => {
-    console.log("Server running on port " + PORT);
-    connectDB();
-})
+  console.log("Server running on port " + PORT);
+  connectDB();
+});
